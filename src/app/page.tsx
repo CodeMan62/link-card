e4324cce75369a2b7e97ef9fbe6b9cc592162462
CodeMan62 <sharmahimanshu15082007@gmail.com>
2025-04-30 18:38:30 +0530

    basic structure for first two forms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+import { ProfileForm } from "@/components/forms/profile-form"
+import SomethingForm from "@/components/forms/something-form"
+import { ModeToggle } from "@/components/theme-toggle" 
 export default function Home() {
   return (
     <div>
+      <ModeToggle />
+      <ProfileForm />
+      <SomethingForm />
     </div>
   )
 }
