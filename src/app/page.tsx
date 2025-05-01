b631ccba364f727c58fd6786bf1c390fffda70db
CodeMan62 <sharmahimanshu15082007@gmail.com>
2025-05-01 22:59:59 +0530

    feat: navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-import { ProfileForm } from "@/components/forms/profile-form"
-import SomethingForm from "@/components/forms/something-form"
-import { ModeToggle } from "@/components/theme-toggle" 
+import { ModeToggle } from "@/components/theme-toggle"
 export default function Home() {
   return (
-    <div>
-      <ModeToggle />
-      <ProfileForm />
-      <SomethingForm />
+    <div className="container mx-auto py-5 px-8">
+      <div className="flex items-center justify-between">
+        <h1 className="font-bold text-4xl">Link-Card</h1>
+        <ModeToggle />
+      </div>
     </div>
   )
 }
